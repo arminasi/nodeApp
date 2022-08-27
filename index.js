990d65c8c3e14c159b2f434a207f61979442de48
arminasi <armanminasyanc@gmail.com>
2022-08-27 10:50:33 +0400

    was connected with mysql

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const http = require('http');
-const {ALL_USERS} = require('./fakeData.js')
+var mysql = require('mysql');
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'users'
+});
+ 
+connection.connect()
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 	console.log(req.url)
-  if(req.url === '/users') {
-    res.end(JSON.stringify(ALL_USERS))
-  } else if(req.url?.startsWith("/user/")) {
-		const strArr = req.url.split("/");
-		const userId = strArr[strArr.length - 1];
-		const user = ALL_USERS.find(item => item.id === +userId);
-		console.log(user)
-		res.end(JSON.stringify(user));
-	}
-	else {
-		res.end(JSON.stringify('Not Found'))
-	}
+	connection.query('SELECT * from user;', function (error, results, fields) {
+		if(req.url === "/users") {
+			if (error) throw error;
+			res.end(JSON.stringify(results))
+		} else if(req.url === ("/user/sorted")) {
+			const user = results.sort((a, b) => a.age - b.age);
+			res.end(JSON.stringify(user));
+		} else if(req.url?.startsWith("/user/")) {
+			const strArr = req.url.split("/");
+			const socialMedia = strArr[strArr.length - 1];
+			const user = results.filter(item => item.socialMedia === socialMedia);
+			res.end(JSON.stringify(user));
+		}
+	});
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const container = document.getElementById('container');
 const users = document.getElementById('users');
 const BASE_URL = 'http://localhost:3000';
 const user = document.querySelector("#user");
-const inputEl = document.querySelector("#userName")
+const inputEl = document.querySelector("#userInput");
+const sortBtn = document.querySelector("#sortBtn");
+
+sortBtn.addEventListener("click", () => {
+	sortUsers();
+})
 
 users.addEventListener('click', () => {
    getUsers()
@@ -12,40 +17,52 @@ user.addEventListener("click", () => {
 	getUser()
 })
 
+async function sortUsers() {
+	const response = await fetch(`${BASE_URL}/user/sorted`)
+	const userData = await response.json()
+	drawUsers(userData)
+}
+
 async function getUser() {
 	if(!inputEl.value) {
 		return;
 	}
 	else {
-		const response =   await  fetch(`${BASE_URL}/user/${inputEl.value}`)
-		const userData =   await response.json()
-		drawUser(userData)
+		const response = await  fetch(`${BASE_URL}/user/${inputEl.value}`)
+		const userData = await response.json()
+		drawUsers(userData)
 	}
-
-}
-
-function drawUser({name, id}) {
-	container.innerHTML = "";
-	const divId = document.createElement("div");
-	const divName = document.createElement("div");
-
-	divName.append(`Name : ${name}`)
-	divId.append(`Id : ${id}`)
-
-	container.append(divName, divId);
 }
 
 async function getUsers() {
-  const response =   await  fetch(`${BASE_URL}/users`)
-  const userData =   await response.json()
+  const response = await  fetch(`${BASE_URL}/users`)
+  const userData = await response.json()
   drawUsers(userData)
 }
+
 function drawUsers(data) {
     container.innerHTML = '';
-    data.forEach(({name}) => {
-        const div = document.createElement('div');
-        div.append(name);
-        container.append(div)
+
+    data.forEach(({name, age, socialMedia}, index) => {
+			setTimeout(() => {
+				const frameEl = document.createElement("div");
+				frameEl.setAttribute("class", "userFrame");
+
+				const nameEl = document.createElement("div");
+				nameEl.setAttribute("class", "userName");
+				nameEl.innerText = name;
+
+				const ageEl = document.createElement("div");
+				ageEl.setAttribute("class", "userAge");
+				ageEl.innerText = age;
+				
+				const socialMediaEl = document.createElement("div");
+				socialMediaEl.setAttribute("class", "userSocial");
+				socialMediaEl.innerText = socialMedia;
+
+        frameEl.append(nameEl, ageEl, socialMediaEl);
+        container.append(frameEl)
+			}, index * 24)
     });
 }
 
